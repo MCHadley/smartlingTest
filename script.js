be05585ca641c4d39bc401c829154dc0f51b1b0d
Michael <michael.c.hadley@gmail.com>
2023-09-03 16:55:25 -0400

    image upload

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const { api } = require('./utils')
 const fs = require('fs');
 const FormData = require('form-data');
 const { getToken } = require('./auth.js');
-const { log } = require('console');
+
+const projectId = '2160f45b8'
 
 const stringUpload = async (event, context) => {
-    const projectId = '2160f45b8'
     const stringForm = new FormData();
     stringForm.append('file', fs.createReadStream('./localizable.strings'))
     stringForm.append('fileUri', 'translationFile')
@@ -22,13 +22,38 @@ const stringUpload = async (event, context) => {
                 "content-type": "multipart/formdata"
             }
         })
+
         if (fileUpload.data.response.code === 'SUCCESS') {
-            console.log("success");
+            console.log(fileUpload.data.response);
         }
-        return fileUpload
     } catch (error) {
         console.error(`There was an error: ${error}`)
     }
 }
 
-stringUpload();
+const imageUpload = async (event, conetext) => {
+    const imageForm = new FormData();
+    imageForm.append('name', 'IMAGE')
+    imageForm.append('content', fs.createReadStream('./context.png'))
+    const uploadURL = api(`context-api/v2/projects/${projectId}/contexts`)
+    try {
+        const token = await getToken()
+        const fileUpload = await axios.postForm(uploadURL, imageForm, {
+            headers: {
+                ...imageForm.getHeaders(),
+                Authorization: `Bearer ${token}`,
+                "content-type": "multipart/form-data"
+            }
+        })
+
+        if (fileUpload.data.response.code === 'SUCCESS') {
+            console.log(fileUpload.data.response);
+        }
+
+    } catch (error) {
+        console.error(`There was an error: ${error}`)
+    }
+}
+
+// stringUpload();
+imageUpload();
